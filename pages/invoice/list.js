17ea668662b685845505d613f11a5fe7163632fc
Ping Chen <chp0524@gmail.com>
2024-05-22 10:18:28 +0800

    remove invoice

--- pages/invoice/list.js
+++ pages/invoice/list.js
@@ -1,73 +1,66 @@
-const WXAPI = require('apifm-wxapi')
-const AUTH = require('../../utils/auth')
-
+// pages/invoice/list.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    invoiceList: []
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad(options) {
 
   },
-  onShow: function () {
-    const _this = this
-    AUTH.checkHasLogined().then(isLogined => {
-      if (isLogined) {
-        WXAPI.invoiceList({
-          token: wx.getStorageSync('token')
-        }).then(res => {
-          if (res.code == 0) {
-            _this.setData({
-              invoiceList: res.data.result
-            })
-          } else {
-            _this.setData({
-              invoiceList: []
-            })
-          }
-        })
-      } else {
-        wx.showModal({
-          title: '提示',
-          content: '本次操作需要您的登录授权',
-          cancelText: '暂不登录',
-          confirmText: '前往登录',
-          success(res) {
-            if (res.confirm) {
-              wx.switchTab({
-                url: "/pages/my/index"
-              })
-            } else {
-              wx.navigateBack()
-            }
-          }
-        })
-      }
-    })
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
   },
-  download(e) {
-    const _this = this
-    const file = e.currentTarget.dataset.file
-    wx.downloadFile({
-      url: file,
-      success (res) {
-        const tempFilePath = res.tempFilePath
-        console.log(tempFilePath);
-        wx.openDocument({
-          filePath: tempFilePath,
-          showMenu: true,
-          success: function (res) {
-            console.log('打开文档成功')
-          }
-        })
-      }
-    })
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
   },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
 })
